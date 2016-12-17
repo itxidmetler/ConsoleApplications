c858dd63cb392a799c8981c5db74cd4a578f1900
itxidmetler <SahibCebrayilli3@gmail.com>
2016-12-17 21:00:03 +0400

    latest end

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,11 +4,22 @@ class Program
     static void Main()
     {
         int[] nums = { 12,34,5,1,2 };
-        int[] nums2=new int[nums.Length];
+        //int[] nums2;
+        //int searchNum = 2;
+
+        Array.ForEach(nums, delegate(int x) { x = x + 2; });
+        foreach (int item in nums)
+        {
+            Console.Write(item + " ");
+        }
+
+
+        #region Comments
+        /*
         Array.Copy(nums, nums2, nums.Length);
         foreach (int item in nums2)
         {
-            Console.Write(item+" ");
+            Console.Write(item + " ");
         }
         Console.WriteLine();
         nums2[0] = 0;
@@ -21,6 +32,38 @@ class Program
         {
             Console.Write(item + " ");
         }
+         * ////////////////////////////////////////////////////////////////
+        myclass[] newItems = new myclass[5];
+        newItems[0] = new myclass(5);
+        newItems[1] = new myclass(1);
+        newItems[3] = new myclass(2);
+        newItems[2] = new myclass(3);
+        newItems[4] = new myclass(4);
+        Array.ForEach(newItems, Program.Negate);
+
+        foreach (myclass item in newItems)
+        {
+            Console.Write(item.i+" ");    
+        }
+        */
+        #endregion
+
         Console.ReadLine();
+    }
+    static void Negate(int o)
+    {
+        o *= o;
+    }
+    static void Negate(myclass o)
+    {
+        o.i = -o.i;
+    }
+}
+class myclass
+{
+    public int i;
+    public myclass(int i)
+    {
+        this.i = i;
     }
 }
