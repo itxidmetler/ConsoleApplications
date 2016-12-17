1714985fe5e7c6bc203952983897116b0052f9d3
itxidmetler <SahibCebrayilli3@gmail.com>
2016-12-17 18:42:12 +0400

    latest end

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,7 +3,24 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello World!");
+        int[] nums = { 12,34,5,1,2 };
+        int[] nums2=new int[nums.Length];
+        Array.Copy(nums, nums2, nums.Length);
+        foreach (int item in nums2)
+        {
+            Console.Write(item+" ");
+        }
+        Console.WriteLine();
+        nums2[0] = 0;
+        foreach (int item in nums)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+        foreach (int item in nums2)
+        {
+            Console.Write(item + " ");
+        }
         Console.ReadLine();
     }
 }
