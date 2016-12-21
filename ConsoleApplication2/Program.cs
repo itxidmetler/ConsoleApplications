317344198fc7398160805c000d0838eb8a940035
itxidmetler <SahibCebrayilli3@gmail.com>
2016-12-21 04:06:56 +0400

    Latest commit

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Text;
 
 class Program
 {
     static void Main()
     {
-        
+        String s = new String('a', Console.WindowWidth);
+        Console.WriteLine(s);
+        Console.ReadLine();
     }
 }
